b17658bd38d08818a6eb5e148d42697f2942dad9
Parul Johri <48296846+paruljohri@users.noreply.github.com>
2022-04-30 10:34:02 -0400

    Update generate_sampling_scheme_demography.py

--- ABCScriptsNeutralDemography/generate_sampling_scheme_demography.py
+++ ABCScriptsNeutralDemography/generate_sampling_scheme_demography.py
@@ -1,6 +1,6 @@
 #This is to generate the sampling scheme for Nanc, Ncur and Time:
 #N is picked with loguniform distribution between upper and lower bound
-#Scaling factor is calculated separately for each set of parameter combinations, where minimum popn size is 1000
+
 import random
 import sys
 import math
@@ -22,13 +22,6 @@ def get_growth_factor(N_anc, N_cur):
         growth_factor = 1.0 + g
     return growth_factor
 
-def get_scaling_factor(N_anc, N_cur):
-    N_min = min(N_anc, N_cur)
-    scaling_factor = int(float(N_min)/5000)
-    if scaling_factor > 200:
-        scaling_factor = 200
-    return scaling_factor
-
 def sample_loguniform(lower_bound, upper_bound):
     N_size = round(math.exp(random.uniform(math.log(lower_bound), math.log(upper_bound))))
     return (N_size)
@@ -45,9 +38,6 @@ while simID <= int(endingID):
     N_anc = sample_loguniform(N_range_lower, N_range_upper)
     N_cur = sample_loguniform(N_range_lower, N_range_upper)
     time = sample_loguniform(10, N_cur)
-    #scaling_factor = get_scaling_factor(t_N[0], t_N[1])
-    #Na = round(t_N[0]/float(scaling_factor))
-    #Nc = round(t_N[1]/float(scaling_factor))
     
     result.write("sim" + str(simID) + '\t' + str(N_anc) + '\t' + str(N_cur) + '\t' + str(time) + '\n')
     simID = simID + 1
